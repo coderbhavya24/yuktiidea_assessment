1ff5d18f76c8378166b1ee9de60ce314b64900e9
coderbhavya24 <bhavya2015340@akgec.ac.in>
2024-04-19 16:58:53 +0530

    working fine

--- lib/Terms and Conditions.dart
+++ lib/Terms and Conditions.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
+// import 'package:flutter_html/flutter_html.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:http/http.dart' as http;
